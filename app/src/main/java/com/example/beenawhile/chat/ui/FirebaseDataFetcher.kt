36a24ac43b908db9707c2a76a91b0789d05b0c9f
LockDoWN_IN <lockdown0502@gmail.com>
2023-12-03 19:52:42 +0900

    채팅내역이 안나눠짐

--- app/src/main/java/com/example/beenawhile/chat/ui/FirebaseDataFetcher.kt
+++ app/src/main/java/com/example/beenawhile/chat/ui/FirebaseDataFetcher.kt
@@ -1,12 +1,8 @@
 package com.example.beenawhile.chat.ui
+
 import android.util.Log
-import androidx.compose.runtime.MutableState
-import androidx.lifecycle.MutableLiveData
-import chatRoomId
-import com.example.beenawhile.chat.data.Conversation
 import com.example.beenawhile.chat.data.Message
 import com.google.firebase.database.*
-import com.example.beenawhile.chat.data.ConversationRepository
 
 class FirebaseDataFetcher(private val onDataFetched: (List<Message>) -> Unit) {
 
@@ -21,10 +17,13 @@ class FirebaseDataFetcher(private val onDataFetched: (List<Message>) -> Unit) {
                 // 데이터베이스의 모든 자식 노드를 가져옴
                 snapshot.children.forEach { childSnapshot ->
                     // 각 자식 노드의 데이터를 가져와 Message 객체로 변환하여 리스트에 추가
-                    val message = childSnapshot.getValue(Message::class.java)
-                    message?.let {
-                        dataList.add(it)
-                    }
+                    val messageText = childSnapshot.child("message").getValue(String::class.java)
+                    val time = childSnapshot.child("time").getValue(String::class.java)
+                    val isFromUser = childSnapshot.child("isFromUser").getValue(Boolean::class.java)
+
+                    // Message 객체를 생성하여 리스트에 추가
+                    val message = Message(messageText.orEmpty(), time.orEmpty(), isFromUser ?: false)
+                    dataList.add(message)
                 }
 
                 // UI에 데이터를 전달
@@ -41,8 +40,4 @@ class FirebaseDataFetcher(private val onDataFetched: (List<Message>) -> Unit) {
     fun fetchDataAndUpdate(chatRoomId: String) {
         fetchData(chatRoomId)
     }
-
-
 }
-
-
